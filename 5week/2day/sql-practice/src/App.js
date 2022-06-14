6dccfcc64945413e4544d60a5264db3756c1c497
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-06-14 16:04:40 -0600

    added next and previous buttons to add funtionality

--- 5week/2day/sql-practice/src/App.js
+++ 5week/2day/sql-practice/src/App.js
@@ -3,33 +3,49 @@ import './App.css';
 import Button from '@mui/material/Button'
 import axios from 'axios'
 
+
 function App() {
-  const [users , setusers] = useState([])
+  const [users , setusers] = useState()
   const [first, setFirst] = useState()
+  let [id, setId] = useState(0)
   
 const getStuff = () => {
   axios
   .get(`http://localhost:4004/stuff`)
   .then(res => {
-    for (let i = 0; i < res.data.length; i++) {
-      if(users){
-         setusers([])
-      }
-      setusers( res.data[i])
-
-    }
-    // setusers(res.data)
-    setFirst(res.data[0].first_names)
+    // console.log(res.data)
+    setusers(res.data)
   })
-  console.log(users)
 }
-
+const next = (e) => {
+  e.preventDefault()
+  setId(id += 1)
+}
+const pre = (e) => {
+  e.preventDefault()
+  setId(id -= 1)
+}
+console.log(users)
   return (
     <div className="App">
+      <Button variant="outlined" color="primary" size='large' onClick={getStuff}>
+        get users
+      </Button> <br />
+      {users && users[id].first_names}
+      <br />
+      {users && users[id].last_names}
+      <br />
+      {users && users[id].age}
+      <br />
+      {users && users[id].job}
        <br />
-      <Button variant="contained" color="primary" size='medium' onClick={getStuff}>
-        hello
-      </Button>
+      <br />
+      {id > 0 ? <Button variant="contained" color="secondary" size='small' onClick={pre}>
+        previous
+      </Button> : <></> }
+      {id < 4 ? <Button variant="contained" color="secondary" size='small' onClick={next}>
+        next
+      </Button> : <></>}
     </div>
   );
 }
