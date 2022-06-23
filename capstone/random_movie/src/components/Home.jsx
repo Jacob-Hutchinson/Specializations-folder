2442eb0214e56a3a43786a564b4d94f4196e15a0
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-06-22 18:01:05 -0600

    added about section

--- capstone/random_movie/src/components/Home.jsx
+++ capstone/random_movie/src/components/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/home.css'
 
 export const Home = () => {
+  useEffect(() => {
+
+  }, [])
   return (
     <div className='homepage'>
-        home
+        <h1>Welcome to the Movie and Food Generator</h1>
+        <p>You can use this website to look for popular movie selections or find delicious food sugestions. I had a hard time Deciding between lots of movie options and being confident that it was a good one. thus Movie and Food Generator was born. I can now use this website to select one of the top 5000 movies in the United States of America or narrow down the selection by adding a genre and time frame. this is helpful if you are looking for a comedy from the 90's or a action from the 80's. This website has helped made selecting a movie much more effecient. I love it/use and hope you will to!</p>
     </div>
   )
 }
