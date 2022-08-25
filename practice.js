78647d897c25d552afd085ef1bbb59e5f44b03f2
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-08-25 11:57:11 -0600

    adding javascript practice file

---
+++ practice.js
@@ -0,0 +1,12 @@
+// code wars practice in javascript
+
+//kuy 8
+// function digitize(n) {
+//   return n
+//     .toString()
+//     .split("")
+//     .reverse()
+//     .map((number) => +number);
+// }
+
+//kuy
