648ccfe7cec09f9223469a0184981fa3e6139d5c
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-09-05 14:57:38 -0600

    completed Square Every Digit in codewars

--- practice.js
+++ practice.js
@@ -17,3 +17,15 @@
 //     return false;
 //   }
 // }
+
+// kuy 7
+// function squareDigits(num) {
+//   return +num
+//     .toString()
+//     .split("")
+//     .map((num) => {
+//       return num * num;
+//     })
+//     .join("");
+// }
+// console.log(squareDigits(9119));
