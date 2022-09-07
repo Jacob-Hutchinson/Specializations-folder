10514610e3594885d8b934b1cb3fa2017d3b7ee6
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-09-07 14:40:17 -0600

    codewars

--- practice.js
+++ practice.js
@@ -157,3 +157,17 @@
 //     ]
 //   )
 // );
+
+//kuy 8
+// const quarterOf = (month) => {
+//   if (month <= 3) {
+//     return 1;
+//   } else if (month <= 6) {
+//     return 2;
+//   } else if (month <= 9) {
+//     return 3;
+//   } else if (month <= 12) {
+//     return 4;
+//   }
+// };
+// console.log(quarterOf(11));
