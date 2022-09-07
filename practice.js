162a6727dda19b85aebc9f1e5f4b97d79196d25e
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-09-06 22:16:47 -0600

    codewars problem

--- practice.js
+++ practice.js
@@ -102,3 +102,16 @@
 //   return answer;
 // }
 // console.log(nbDig(25, 0));
+
+//kuy 8
+// var min = function (list) {
+//   return Math.min(...list);
+// };
+
+// var max = function (list) {
+//   return Math.max(...list);
+// };
+// console.log(
+//   min([4, 6, 2, 1, 9, 63, -134, 566]),
+//   max([4, 6, 2, 1, 9, 63, -134, 566])
+// );
