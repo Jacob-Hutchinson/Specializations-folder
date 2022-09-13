4a5050ebd365b2110c8941cdedf5232612e7704c
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-09-13 12:43:19 -0600

    codewars

--- practice.js
+++ practice.js
@@ -310,3 +310,46 @@
 //   return x.map((num) => +num).reduce((pre, cur) => pre + cur, 0);
 // }
 // console.log(sumMix([9, 3, "7", "3"]));
+
+//kuy 8
+// function switchItUp(number) {
+//   switch (number) {
+//     case 0:
+//       number = "Zero";
+//       break;
+//     case 1:
+//       number = "One";
+//       break;
+//     case 2:
+//       number = "Two";
+//       break;
+//     case 3:
+//       number = "Three";
+//       break;
+//     case 4:
+//       number = "Four";
+//       break;
+//     case 5:
+//       number = "Five";
+//       break;
+//     case 6:
+//       number = "Six";
+//       break;
+//     case 7:
+//       number = "Seven";
+//       break;
+//     case 8:
+//       number = "Eight";
+//       break;
+//     case 9:
+//       number = "Nine";
+//       break;
+
+//     default:
+//       break;
+//   }
+//   return number;
+// }
+// console.log(switchItUp(2));
+
+//kuy
